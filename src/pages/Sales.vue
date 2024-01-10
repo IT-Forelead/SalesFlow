d8c397398142bf82c0a4eb96dfb782cd913534af
surojiddin <isurojiddin@gmail.com>
2024-01-10 17:03:28 +0500

    sales page maybe finished

--- src/pages/Sales.vue
+++ src/pages/Sales.vue
@@ -4,19 +4,23 @@ import MinusIcon from '../assets/icons/MinusIcon.vue';
 import PlusIcon from '../assets/icons/PlusIcon.vue';
 import TrashIcon from '../assets/icons/TrashIcon.vue';
 import SearchIcon from '../assets/icons/SearchIcon.vue';
+import MoneyIcon from '../assets/icons/MoneyIcon.vue';
+import CreditCardIcon from '../assets/icons/CreditCardIcon.vue';
 
 
 </script>
 
 <template>
   <div class="grid grid-cols-3 gap-4">
-    <div class="col-span-2">
+    <div class="col-span-2 bg-white rounded-md py-6 px-4 space-y-4">
       <div class="flex items-center justify-between">
         <div class="relative w-96">
           <div class="absolute inset-y-0 left-0 flex items-center pl-2 pointer-events-none">
             <SearchIcon class="w-6 h-6 text-gray-500 dark:text-gray-400" />
           </div>
-          <input type="text" class="bg-white border border-gray-300 text-gray-900 text-md rounded focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 px-3 py-2  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Mahsulot nomi bo'yicha izlash...">
+          <input type="text"
+            class="bg-white border border-gray-300 text-gray-900 text-md rounded focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 px-3 py-2  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Mahsulot nomi bo'yicha izlash...">
           <button type="button" class="absolute inset-y-0 right-0 px-3 bg-[#0167F3] text-white rounded-r">
             Izlash
           </button>
@@ -42,20 +46,23 @@ import SearchIcon from '../assets/icons/SearchIcon.vue';
                   </div>
                   <div>
                     <div class="text-base font-semibold text-gray-800">Jacobs Monarch 500g</div>
-                    <div class="text-base font-medium text-gray-500">Narxi: <span class="text-gray-700">158 000 so'm</span>
+                    <div class="text-base font-medium text-gray-500">Narxi: <span class="text-gray-700">158 000
+                        so'm</span>
                     </div>
                   </div>
                 </td>
                 <td class="p-3 text-center whitespace-nowrap">
                   <div class="flex justify-center">
                     <div class="flex items-center justify-between w-28 rounded-full shadow">
-                      <div class="flex items-center justify-center w-8 h-8 text-gray-700 hover:text-[#0167F3] hover:bg-gray-100 rounded-full">
+                      <div
+                        class="flex items-center justify-center w-8 h-8 text-gray-700 hover:text-[#0167F3] hover:bg-gray-100 rounded-full">
                         <MinusIcon class="w-4 h-4" />
                       </div>
                       <div class="flex items-center justify-center text-lg font-normal">
                         2
                       </div>
-                      <div class="flex items-center justify-center w-8 h-8 text-gray-700 hover:text-[#0167F3] hover:bg-gray-100 rounded-full">
+                      <div
+                        class="flex items-center justify-center w-8 h-8 text-gray-700 hover:text-[#0167F3] hover:bg-gray-100 rounded-full">
                         <PlusIcon class="w-4 h-4" />
                       </div>
                     </div>
@@ -66,7 +73,7 @@ import SearchIcon from '../assets/icons/SearchIcon.vue';
                 </td>
                 <td class="p-3 whitespace-nowrap">
                   <div class="flex justify-center">
-                    <TrashIcon class="w-6 h-6 text-rose-500 cursor-pointer" />
+                    <TrashIcon class="w-6 h-6 text-rose-500 cursor-pointer transform hover:scale-105" />
                   </div>
                 </td>
               </tr>
@@ -84,13 +91,15 @@ import SearchIcon from '../assets/icons/SearchIcon.vue';
                 <td class="p-3 text-center whitespace-nowrap">
                   <div class="flex justify-center">
                     <div class="flex items-center justify-between w-28 rounded-full shadow">
-                      <div class="flex items-center justify-center w-8 h-8 text-gray-700 hover:text-[#0167F3] hover:bg-gray-100 rounded-full">
+                      <div
+                        class="flex items-center justify-center w-8 h-8 text-gray-700 hover:text-[#0167F3] hover:bg-gray-100 rounded-full">
                         <MinusIcon class="w-4 h-4" />
                       </div>
                       <div class="flex items-center justify-center text-lg font-normal">
                         1
                       </div>
-                      <div class="flex items-center justify-center w-8 h-8 text-gray-700 hover:text-[#0167F3] hover:bg-gray-100 rounded-full">
+                      <div
+                        class="flex items-center justify-center w-8 h-8 text-gray-700 hover:text-[#0167F3] hover:bg-gray-100 rounded-full">
                         <PlusIcon class="w-4 h-4" />
                       </div>
                     </div>
@@ -101,17 +110,97 @@ import SearchIcon from '../assets/icons/SearchIcon.vue';
                 </td>
                 <td class="p-3 whitespace-nowrap">
                   <div class="flex justify-center">
-                    <TrashIcon class="w-6 h-6 text-rose-500 cursor-pointer" />
+                    <TrashIcon class="w-6 h-6 text-rose-500 cursor-pointer transform hover:scale-105" />
                   </div>
                 </td>
               </tr>
-          </tbody>
-        </table>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
+
+    <div class="bg-white rounded-md py-2 px-6">
+      <div class="border-b py-2">
+        <h3 class="text-xl font-semibold">Harid tafsilotlari</h3>
+      </div>
+      <div class="border-b py-4">
+        <div class="pb-3 space-y-1">
+          <div class="flex items-center justify-between">
+            <div class="text-base text-gray-600">
+              Mahsulotlar soni
+            </div>
+            <div class="text-base font-semibold text-gray-900">
+              2 ta
+            </div>
+          </div>
+          <div class="flex items-center justify-between">
+            <div class="text-base text-gray-600">
+              Narxi
+            </div>
+            <div class="text-base font-semibold text-gray-900">
+              409 000 so'm
+            </div>
+          </div>
+          <div class="flex items-center justify-between">
+            <div class="text-base text-gray-600">
+              Chegirma
+            </div>
+            <div class="text-base font-semibold text-gray-900">
+              0 %
+            </div>
+          </div>
+          <div class="flex items-center justify-between">
+            <div class="text-base text-gray-600">
+              Chegirma miqdori
+            </div>
+            <div class="text-base font-semibold text-red-500">
+              -0 so'm
+            </div>
+          </div>
+        </div>
+        <div class="flex items-center justify-between mt-2 pt-3 border-t border-dashed">
+          <div class="text-xl font-semibold text-gray-900">
+            Umumiy
+          </div>
+          <div class="text-xl font-semibold text-gray-900">
+            409 000 so'm
+          </div>
+        </div>
+      </div>
+      <div class="border-b py-3 space-y-1">
+        <label class="text-base font-medium">Qabul qilingan to'lov</label>
+        <money3 v-model="cost" v-bind="moneyConf" id="price"
+          class="border-none text-right text-gray-500 bg-gray-100 rounded-lg w-full text-lg dark:text-gray-300 dark:bg-gray-900"> </money3>
+      </div>
+      <div class="py-3 space-y-1">
+        <div class="text-base font-medium">To'lov turi</div>
+        <div class="flex items-center space-x-4">
+          <div class="flex-1 flex flex-col items-center justify-center bg-blue-50 border border-blue-300 rounded-lg py-4">
+            <MoneyIcon class="w-6 h-6 text-blue-500" />
+            <div class="text-lg font-medium text-blue-500">
+              Naqt pul orqali
+            </div>
+          </div>
+          <div class="flex-1 flex flex-col items-center justify-center border rounded-lg py-4">
+            <CreditCardIcon class="w-6 h-6 text-gray-500" />
+            <div class="text-lg font-medium">
+              Plastik orqali
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="py-4">
+        <button class="w-full py-3 px-4 rounded-full text-white text-lg font-medium bg-blue-500 cursor-pointer hover:bg-blue-600">
+          To'lov
+        </button>
+      </div>
+
+    </div>
+
+
   </div>
-
-
-</div></template>
+</template>
 
 <style scoped></style>
