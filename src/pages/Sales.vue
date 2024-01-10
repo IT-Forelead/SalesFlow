a7f4cf3917896396f2ed5b00dd9dba7fee1b5418
surojiddin <isurojiddin@gmail.com>
2024-01-10 18:00:03 +0500

    product page improved

--- src/pages/Sales.vue
+++ src/pages/Sales.vue
@@ -7,51 +7,53 @@ import SearchIcon from '../assets/icons/SearchIcon.vue';
 import MoneyIcon from '../assets/icons/MoneyIcon.vue';
 import CreditCardIcon from '../assets/icons/CreditCardIcon.vue';
 
-
 </script>
 
 <template>
   <div class="grid grid-cols-3 gap-4">
-    <div class="col-span-2 bg-white rounded-md py-6 px-4 space-y-4">
+    <div class="col-span-2 bg-white rounded-lg py-6 px-4 space-y-4">
       <div class="flex items-center justify-between">
         <div class="relative w-96">
           <div class="absolute inset-y-0 left-0 flex items-center pl-2 pointer-events-none">
             <SearchIcon class="w-6 h-6 text-gray-500 dark:text-gray-400" />
           </div>
           <input type="text"
-            class="bg-white border border-gray-300 text-gray-900 text-md rounded focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 px-3 py-2  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            class="bg-white border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 px-3 py-2  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Mahsulot nomi bo'yicha izlash...">
-          <button type="button" class="absolute inset-y-0 right-0 px-3 bg-[#0167F3] text-white rounded-r">
+          <button type="button" class="absolute inset-y-0 right-0 px-3 bg-[#0167F3] text-white rounded-r-lg">
             Izlash
           </button>
         </div>
       </div>
 
       <div class="inline-block min-w-full py-2 align-middle">
-        <div class="overflow-hidden rounded-sm ring-1 ring-black ring-opacity-5">
+        <div class="overflow-hidden rounded-lg border border-gray-200">
           <table class="min-w-full divide-y divide-gray-200">
-            <thead class="bg-white">
+            <thead class="bg-gray-50">
               <tr>
-                <th scope="col" class="p-3 text-lg font-semibold text-left text-gray-900">Mahsulot</th>
-                <th scope="col" class="p-3 text-lg font-semibold text-center text-gray-900">Miqdori</th>
-                <th scope="col" class="p-3 text-lg font-semibold text-center text-gray-900">Jami narxi</th>
-                <th scope="col" class="p-3 text-lg font-semibold text-center text-gray-900">Amallar</th>
+                <th scope="col" class="px-3 py-2 text-base font-semibold text-left text-gray-900">Mahsulot</th>
+                <th scope="col" class="px-3 py-2 text-base font-semibold text-center text-gray-900">Miqdori</th>
+                <th scope="col" class="px-3 py-2 text-base font-semibold text-center text-gray-900">Jami narxi</th>
+                <th scope="col" class="px-3 py-2 text-base font-semibold text-center text-gray-900">Amallar</th>
               </tr>
             </thead>
             <tbody class="bg-white divide-y divide-gray-200">
               <tr>
                 <td class="flex items-center px-3 py-2 space-x-3 whitespace-nowrap">
-                  <div class="flex items-center justify-center bg-gray-100 w-12 h-12 rounded-md">
+                  <div class="flex items-center justify-center bg-gray-100 w-12 h-12 rounded-lg">
                     <ImageIcon class="text-gray-500 w-8 h-8" />
                   </div>
                   <div>
                     <div class="text-base font-semibold text-gray-800">Jacobs Monarch 500g</div>
-                    <div class="text-base font-medium text-gray-500">Narxi: <span class="text-gray-700">158 000
-                        so'm</span>
+                    <div class="text-base font-medium text-gray-500">
+                      Narxi:
+                      <span class="text-gray-700">
+                        158 000 so'm
+                      </span>
                     </div>
                   </div>
                 </td>
-                <td class="p-3 text-center whitespace-nowrap">
+                <td class="px-3 py-2 text-center whitespace-nowrap">
                   <div class="flex justify-center">
                     <div class="flex items-center justify-between w-28 rounded-full shadow">
                       <div
@@ -68,10 +70,10 @@ import CreditCardIcon from '../assets/icons/CreditCardIcon.vue';
                     </div>
                   </div>
                 </td>
-                <td class="p-3 text-center whitespace-nowrap">
+                <td class="px-3 py-2 text-center whitespace-nowrap">
                   316 000 so'm
                 </td>
-                <td class="p-3 whitespace-nowrap">
+                <td class="px-3 py-2 whitespace-nowrap">
                   <div class="flex justify-center">
                     <TrashIcon class="w-6 h-6 text-rose-500 cursor-pointer transform hover:scale-105" />
                   </div>
@@ -79,7 +81,7 @@ import CreditCardIcon from '../assets/icons/CreditCardIcon.vue';
               </tr>
               <tr>
                 <td class="flex items-center px-3 py-2 space-x-3 whitespace-nowrap">
-                  <div class="flex items-center justify-center bg-gray-100 w-12 h-12 rounded-md">
+                  <div class="flex items-center justify-center bg-gray-100 w-12 h-12 rounded-lg">
                     <ImageIcon class="text-gray-500 w-8 h-8" />
                   </div>
                   <div>
@@ -88,7 +90,7 @@ import CreditCardIcon from '../assets/icons/CreditCardIcon.vue';
                     </div>
                   </div>
                 </td>
-                <td class="p-3 text-center whitespace-nowrap">
+                <td class="px-3 py-2 text-center whitespace-nowrap">
                   <div class="flex justify-center">
                     <div class="flex items-center justify-between w-28 rounded-full shadow">
                       <div
@@ -105,11 +107,11 @@ import CreditCardIcon from '../assets/icons/CreditCardIcon.vue';
                     </div>
                   </div>
                 </td>
-                <td class="p-3 text-center whitespace-nowrap">
+                <td class="px-3 py-2 text-center whitespace-nowrap">
                   93 000 so'm
                 </td>
-                <td class="p-3 whitespace-nowrap">
-                  <div class="flex justify-center">
+                <td class="px-3 py-2 whitespace-nowrap">
+                  <div class="flex items-center justify-center">
                     <TrashIcon class="w-6 h-6 text-rose-500 cursor-pointer transform hover:scale-105" />
                   </div>
                 </td>
@@ -120,7 +122,7 @@ import CreditCardIcon from '../assets/icons/CreditCardIcon.vue';
       </div>
     </div>
 
-    <div class="bg-white rounded-md py-2 px-6">
+    <div class="bg-white rounded-lg py-2 px-6">
       <div class="border-b py-2">
         <h3 class="text-xl font-semibold">Harid tafsilotlari</h3>
       </div>
