bdb454047419b1c6a1b8c3937f075c551c6996e8
surojiddin <isurojiddin@gmail.com>
2024-01-25 11:51:16 +0500

    create user improved

--- src/components/modals/CreateUserModal.vue
+++ src/components/modals/CreateUserModal.vue
@@ -2,16 +2,21 @@
 import { useModalStore } from '../../store/modal.store.js'
 import CModal from '../common/CModal.vue'
 import CancelButton from '../buttons/CancelButton.vue'
-import SaveButton from '../buttons/SaveButton.vue'
 import { reactive, ref } from 'vue'
 import MultiSelect from 'primevue/multiselect'
+import Spinners270RingIcon from '../../assets/icons/Spinners270RingIcon.vue'
+import EyeIcon from '../../assets/icons/EyeIcon.vue'
+import EyeSlashIcon from '../../assets/icons/EyeSlashIcon.vue'
 import UserService from '../../services/user.service.js'
 import { toast } from 'vue-sonner'
 import { useUserStore } from '../../store/user.store.js'
 import { vMaska } from 'maska'
 
 const isLoading = ref(false)
+const hidePassword = ref(true)
 const selectedRole = ref([])
+
+const togglePassword = () => (hidePassword.value = !hidePassword.value)
 
 const privileges = ref([
   { name: 'Kassir', code: ['create_product'] },
@@ -24,6 +29,7 @@ const submitForm = reactive({
   lastname: '',
   login: '',
   password: '',
+  confirmPassword: '',
   privileges: [],
   phone: '',
 })
@@ -44,13 +50,23 @@ const closeModal = () => {
 
 const createUser = () => {
   submitForm.privileges = selectedRole.value.flatMap(role => role.code)
-  if (!submitForm.firstname) return toast.warning('Iltimos ism kiriting')
-  if (!submitForm.lastname) return toast.warning('Iltimos familiya kiriting')
-  if (!submitForm.login) return toast.warning('Iltimos login kiriting')
-  if (!submitForm.password) return toast.warning('Iltimos parol kiriting')
-  if (submitForm.privileges.length === 0) return toast.warning('Iltimos rol tanlang')
-  if (!submitForm.phone) return toast.warning('Iltimos telefon raqam kiriting')
-  else {
+  if (!submitForm.firstname) {
+    toast.warning('Iltimos ismni kiriting')
+  } else if (!submitForm.lastname) {
+    toast.warning('Iltimos familiyani kiriting')
+  } else if (!submitForm.login) {
+    toast.warning('Iltimos loginni kiriting')
+  } else if (!submitForm.phone) {
+    toast.warning('Iltimos telefon raqamni kiriting')
+  } else if (submitForm.privileges.length === 0) {
+    toast.warning('Iltimos rol tanlang')
+  } else if (!submitForm.password) {
+    toast.warning('Iltimos parolni kiriting')
+  } else if (!submitForm.confirmPassword) {
+    toast.warning('Iltimos parolni qayta takrorlang')
+  } else if (submitForm.password !== submitForm.confirmPassword) {
+    toast.warning('Parollar mos kelmadi')
+  } else {
     isLoading.value = true
     const formData = new FormData()
     formData.append('firstname', submitForm.firstname)
@@ -67,8 +83,8 @@ const createUser = () => {
         isLoading.value = false
         UserService.getUsers()
           .then((res) => {
-              useUserStore().clearStore()
-              useUserStore().setUsers(res)
+            useUserStore().clearStore()
+            useUserStore().setUsers(res)
             console.log(useUserStore().users)
           })
           .catch(() => {
@@ -86,105 +102,126 @@ const createUser = () => {
 
 <template>
   <div>
-    <CModal
-      :is-open="useModalStore().isCreateUserModalOpen"
-      v-if="useModalStore().isCreateUserModalOpen"
-      @close=closeModal
-    >
+    <CModal :is-open="useModalStore().isCreateUserModalOpen" v-if="useModalStore().isCreateUserModalOpen"
+      @close=closeModal>
       <template v-slot:header>
         Foydalanuvchi qo'shish
       </template>
       <template v-slot:body>
-        <form @submit.prevent="createUser()" method="post">
-          <div class="grid gap-4 sm:grid-cols-1 sm:gap-6 px-2">
-            <div class="w-full">
-              <label for="firstname"
-                     class="block mb-2 text-neutral-800 text-base font-normal after:text-red-500 after:content-['*']">Ism</label>
-              <input
-                v-model="submitForm.firstname"
-                type="text"
-                name="firstname"
-                id="firstname"
-                class="bg-slate-50 border border-slate-200 text-slate-900 text-base rounded-2xl focus:ring-green-400/40 focus:border-green-400/40 focus:ring-4 block w-full p-2.5"
-                placeholder="Ismingizni kiriting"
-                required=""
-              />
+
+        <div class="space-y-4">
+          <div class="flex items-center space-x-4">
+            <div class="flex-1">
+              <label for="firstname" class="text-base font-medium">
+                Ism
+                <span class="text-red-500 mr-2">*</span>
+              </label>
+              <input id="firstname" type="text" v-model="submitForm.firstname"
+                class="bg-slate-100 border-none text-slate-900 rounded-lg w-full py-2.5 placeholder-slate-400"
+                placeholder="Ismni kiriting">
             </div>
-            <div class="w-full">
-              <label for="lastname" class="text-neutral-800 text-base font-normal after:text-red-500 after:content-['*']">Familiya</label>
-              <input
-                v-model="submitForm.lastname"
-                type="text"
-                name="lastname"
-                id="lastname"
-                class="bg-slate-50 border border-slate-200 text-slate-900 text-base rounded-2xl focus:ring-green-400/40 focus:border-green-400/40 focus:ring-4 block w-full p-2.5"
-                placeholder="Familiyangizni kiriting"
-                required=""
-              />
-            </div>
-            <div class="w-full">
-              <label for="login" class="block mb-2 text-neutral-800 text-base font-normal after:text-red-500 after:content-['*']">Login</label>
-              <input
-                v-model="submitForm.login"
-                type="text"
-                name="login"
-                id="login"
-                class="bg-slate-50 border border-slate-200 text-slate-900 text-base rounded-2xl focus:ring-green-400/40 focus:border-green-400/40 focus:ring-4 block w-full p-2.5"
-                placeholder="Login kiriting" />
-            </div>
-            <div class="w-full">
-              <label for="parol"
-                     class="block mb-2 text-neutral-800 text-base font-normal after:text-red-500 after:content-['*']">Parol</label>
-              <input v-model="submitForm.password"
-                     type="password"
-                     name="parol"
-                     id="parol"
-                     class="bg-slate-50 border border-slate-200 text-slate-900 text-base rounded-2xl focus:ring-green-400/40 focus:border-green-400/40 focus:ring-4 block w-full p-2.5"
-                     placeholder="***"
-              />
-            </div>
-            <div class="w-full">
-              <label for="password"
-                     class="block mb-2 text-neutral-800 text-base font-normal after:text-red-500 after:content-['*']">Rol
-                tanlash</label>
-              <MultiSelect
-                :show-toggle-all="false"
-                :display="'chip'"
-                :select-all="false"
-                panel-class="bg-slate-100 rounded-2xl"
-                v-model="selectedRole"
-                :options="privileges"
-                optionLabel="name"
-                placeholder="Tanlang"
-                :maxSelectedLabels="1"
-                :selection-limit="1"
-                class="w-full border rounded-2xl bg-slate-100"
-              />
-            </div>
-            <div class="w-full">
-              <label for="phone" class="block mb-2 text-neutral-800 text-base font-normal">Telefon raqam:</label>
-              <input
-                v-maska
-                data-maska="+998(##) ###-##-##"
-                v-model="submitForm.phone"
-                placeholder="+998(00) 000-00-00"
-                type="text"
-                name="phone"
-                id="phone"
-                class="bg-slate-50 border border-slate-200 text-slate-900 text-base rounded-2xl focus:ring-green-400/40 focus:border-green-400/40 focus:ring-4 block w-full p-2.5"
-              >
+            <div class="flex-1">
+              <label for="lastname" class="text-base font-medium">
+                Familiya
+                <span class="text-red-500 mr-2">*</span>
+              </label>
+              <input id="lastname" type="text" v-model="submitForm.lastname"
+                class="bg-slate-100 border-none text-slate-900 rounded-lg w-full py-2.5 placeholder-slate-400"
+                placeholder="Familiyani kiriting">
             </div>
           </div>
-        </form>
+          <div class="flex items-center space-x-4">
+            <div class="flex-1">
+              <label for="login" class="text-base font-medium">
+                Login
+                <span class="text-red-500 mr-2">*</span>
+              </label>
+              <input id="login" type="text" v-model="submitForm.login"
+                class="bg-slate-100 border-none text-slate-900 rounded-lg w-full py-2.5 placeholder-slate-400"
+                placeholder="Loginni kiriting">
+            </div>
+            <div class="flex-1">
+              <label for="phone" class="text-base font-medium">
+                Telefon raqam:
+                <span class="text-red-500 mr-2">*</span>
+              </label>
+              <input id="phone" type="text" v-model="submitForm.phone" v-maska data-maska="+998(##) ###-##-##"
+                class="bg-slate-100 border-none text-slate-900 rounded-lg w-full py-2.5 placeholder-slate-400"
+                placeholder="+998(00) 000-00-00">
+            </div>
+          </div>
+          <div class="flex items-center space-x-4">
+            <div class="flex-1 space-y-1">
+              <label for="default-type" class="text-base font-medium">
+                Do'kon
+              </label>
+              <select id="default-type" class="bg-slate-100 border-none text-slate-900 rounded-lg block w-full h-11">
+                <option selected>Do'konni tanlang</option>
+                <option value="market">Market 1</option>
+                <option value="market">Market 2</option>
+                <option value="market">Market 3</option>
+              </select>
+            </div>
+            <div class="flex-1">
+              <label for="role" class="text-base font-medium">
+                Rol:
+                <span class="text-red-500 mr-2">*</span>
+              </label>
+              <MultiSelect :show-toggle-all="false" :display="'chip'" :select-all="false"
+                panel-class="bg-slate-100 rounded-2xl" v-model="selectedRole" :options="privileges" optionLabel="name"
+                placeholder="Tanlang" :maxSelectedLabels="1" :selection-limit="1"
+                class="bg-slate-100 border-none text-slate-900 rounded-lg w-full placeholder-slate-400" />
+            </div>
+          </div>
+          <div class="flex items-center space-x-4">
+            <div class="flex-1">
+              <label for="password" class="text-base font-medium">
+                Parol
+                <span class="text-red-500 mr-2">*</span>
+              </label>
+              <div class="relative">
+                <input v-model="submitForm.password" id="password" :type="hidePassword ? 'password' : 'text'"
+                  class="bg-slate-100 border-none text-slate-900 rounded-lg w-full py-2.5 placeholder-slate-400"
+                  placeholder="Parolni kiriting">
+                <EyeIcon v-if="hidePassword" @click="togglePassword()"
+                  class="text-gray-500 dark:text-gray-500 absolute z-10 top-1/2 -translate-y-1/2 right-3 w-5 h-5 cursor-pointer" />
+                <EyeSlashIcon v-else @click="togglePassword()"
+                  class="text-gray-500 dark:text-gray-500 absolute z-10 top-1/2 -translate-y-1/2 right-3 w-5 h-5 cursor-pointer" />
+              </div>
+            </div>
+            <div class="flex-1">
+              <label for="confirm-password" class="text-base font-medium">
+                Parolni takrorlang
+                <span class="text-red-500 mr-2">*</span>
+              </label>
+              <div class="relative">
+                <input v-model="submitForm.confirmPassword" id="confirm-password" :type="hidePassword ? 'password' : 'text'"
+                  class="bg-slate-100 border-none text-slate-900 rounded-lg w-full py-2.5 placeholder-slate-400"
+                  placeholder="Parolni qayta kiriting">
+                <EyeIcon v-if="hidePassword" @click="togglePassword()"
+                  class="text-gray-500 dark:text-gray-500 absolute z-10 top-1/2 -translate-y-1/2 right-3 w-5 h-5 cursor-pointer" />
+                <EyeSlashIcon v-else @click="togglePassword()"
+                  class="text-gray-500 dark:text-gray-500 absolute z-10 top-1/2 -translate-y-1/2 right-3 w-5 h-5 cursor-pointer" />
+              </div>
+            </div>
+          </div>
+        </div>
       </template>
       <template v-slot:footer>
-        <SaveButton :isLoading="isLoading" @click="createUser" />
         <CancelButton @click="closeModal" />
+        <button v-if="isLoading" type="bSearchIconutton"
+          class="inline-flex items-center justify-center ms-3 text-white bg-blue-600 focus:ring-4 focus:outline-none focus:ring-slate-300 rounded-xl border border-slate-200 text-sm font-medium px-5 py-2.5 focus:z-10 cursor-default">
+          <Spinners270RingIcon
+            class="mr-2 w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300" />
+          Yaratish
+        </button>
+        <button v-else @click="createUser()" type="button"
+          class="ms-3 text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-slate-300 rounded-xl border border-slate-200 text-sm font-medium px-5 py-2.5 focus:z-10">
+          Yaratish
+        </button>
       </template>
     </CModal>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
