f17765de472f8e754e43724984b4de9d71cf48e8
surojiddin <isurojiddin@gmail.com>
2024-01-26 11:43:35 +0500

    get orders connected with backend

--- src/services/order.service.js
+++ src/services/order.service.js
@@ -4,6 +4,9 @@ class OrderService {
   async createOrder(data) {
     return AxiosService.post('/order/create', data)
   }
+  async getOrders() {
+    return AxiosService.get('/order')
+  }
 }
 
 export default new OrderService()
