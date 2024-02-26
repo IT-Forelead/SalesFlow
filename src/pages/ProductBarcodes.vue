6790cdd9431cc562718ca3bf93c40cc12987af44
surojiddin <isurojiddin@gmail.com>
2024-02-26 13:05:46 +0500

    product barcodes page translated

--- src/pages/ProductBarcodes.vue
+++ src/pages/ProductBarcodes.vue
@@ -11,6 +11,9 @@ import CaretDoubleLeftIcon from '../assets/icons/CaretDoubleLeftIcon.vue'
 import CaretLeftIcon from '../assets/icons/CaretLeftIcon.vue'
 import BarcodesTable from '../components/common/BarcodesTable.vue'
 import EditIcon from '../assets/icons/EditIcon.vue'
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
 
 const globalSearchFromTable = ref('')
 const isLoading = ref(false)
@@ -32,7 +35,7 @@ const openEditProductBarcodeModal = (data) => {
 const columns = [
   {
     accessorKey: 'id',
-    header: '#',
+    header: t('n'),
     enableSorting: false,
     cell: ({ row }) => `${parseInt(row.id, 10) + 1}`,
   },
@@ -47,19 +50,19 @@ const columns = [
   // },
   {
     accessorKey: 'trademark',
-    header: 'Savdo belgisi',
+    header: t('name'),
   },
   {
     accessorKey: 'packaging',
-    header: 'Qadoqi',
+    header: t('package'),
   },
   {
     accessorKey: 'type_code',
-    header: 'Kod turi',
+    header: t('barcodeType'),
   },
   {
     accessorKey: 'barcode',
-    header: 'Shtrix kodi',
+    header: t('barcode'),
   },
   // {
   //   accessorKey: 'reg_number',
@@ -67,19 +70,20 @@ const columns = [
   // },
   {
     accessorKey: 'year',
-    header: 'Yil',
+    header: t('year'),
   },
   {
     accessorKey: 'actions',
-    header: 'Amallar',
+    header: t('actions'),
     cell: ({ row }) => h('div', { class: 'flex justify-center items-center space-x-2' }, [
-      h('button', { onClick: () => {openEditProductBarcodeModal(row.original)} }, [
-        h(EditIcon, {class: 'w-6 h-6 text-blue-600 hover:scale-105'})
+      h('button', { onClick: () => { openEditProductBarcodeModal(row.original) } }, [
+        h(EditIcon, { class: 'w-6 h-6 text-blue-600 hover:scale-105' })
       ]),
     ]),
     enableSorting: false,
   },
 ]
+
 const page = ref(1)
 const pageSize = 30
 const getBarcodes = () => {
@@ -90,10 +94,9 @@ const getBarcodes = () => {
       useProductStore().barcodesTotal = response.total
       useProductStore().setProductBarcodes(response.data)
     }).finally(() => {
-    isLoading.value = false
-  })
+      isLoading.value = false
+    })
 }
-
 
 const totalPages = computed(() => Math.ceil(total.value / pageSize))
 const displayedPageNumbers = computed(() => {
@@ -129,20 +132,22 @@ watch(page, () => {
 </script>
 <template>
   <div class="p-4 md:p-8">
-    <div class="text-slate-900 text-2xl md:text-3xl font-semibold mb-6">Shtrix kodlar</div>
+    <div class="text-slate-900 text-2xl md:text-3xl font-semibold mb-6">
+      {{ $t('barcodes') }}
+    </div>
     <div class="flex flex-col md:flex-row items-center justify-between">
       <div class="relative w-full md:w-auto my-2 md:mb-0 order-2 md:order-1">
         <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
           <SearchIcon class="w-5 h-5 text-slate-400" />
         </div>
         <input type="search" v-model="globalSearchFromTable"
-               class="bg-slate-100 border-none w-full text-slate-900 text-base md:text-lg rounded-full block pl-10 py-2 placeholder-slate-400"
-               placeholder="Search everything..." />
+          class="bg-slate-100 border-none w-full text-slate-900 text-base md:text-lg rounded-full block pl-10 py-2 placeholder-slate-400"
+          placeholder="Search everything..." />
       </div>
       <div class="w-full md:w-auto order-1 md:order-2">
         <button @click="useModalStore().openCreateProductBarcodeModal()"
-                class="w-full md:w-auto py-2 px-4 rounded-full text-white text-lg font-medium bg-blue-500 cursor-pointer hover:bg-blue-600">
-          Shtrix kod qo'shish
+          class="w-full md:w-auto py-2 px-4 rounded-full text-white text-lg font-medium bg-blue-500 cursor-pointer hover:bg-blue-600">
+          {{ $t('addBarcode') }}
         </button>
       </div>
     </div>
@@ -153,47 +158,34 @@ watch(page, () => {
 
     <div class="flex items-center justify-between my-6">
       <div class="text-base text-slate-900 font-medium">
-        Jami:
+        {{ $t('total') }}:
         {{ total }}
       </div>
       <div class="flex items-center space-x-2">
-        <button
-          :disabled="page === 1"
-          @click="goToPage(1)"
+        <button :disabled="page === 1" @click="goToPage(1)"
           class="flex items-center justify-center px-3 py-2 text-base font-medium text-slate-900 rounded-lg select-none hover:bg-blue-200 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
           type="button">
           <CaretDoubleLeftIcon class="w-5 h-5" />
         </button>
-        <button
-          @click="prevPage"
-          :disabled="page === 1"
+        <button @click="prevPage" :disabled="page === 1"
           class="flex items-center justify-center px-3 py-2 text-base font-medium text-slate-900 rounded-lg select-none hover:bg-blue-200 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
           type="button">
           <CaretLeftIcon class="w-5 h-5" />
         </button>
         <div class="flex items-center space-x-2">
-          <button
-            v-for="pageNumber in displayedPageNumbers"
-            :key="pageNumber"
-            @click="goToPage(pageNumber)"
-            :class="{
-        'bg-blue-600 text-white': pageNumber === page,
-        'hover:bg-blue-200': pageNumber !== page,
-      }"
-            class="px-3 py-2 select-none rounded-lg text-slate-900 text-center text-base font-medium transition-all">
+          <button v-for="pageNumber in displayedPageNumbers" :key="pageNumber" @click="goToPage(pageNumber)" :class="{
+            'bg-blue-600 text-white': pageNumber === page,
+            'hover:bg-blue-200': pageNumber !== page,
+          }" class="px-3 py-2 select-none rounded-lg text-slate-900 text-center text-base font-medium transition-all">
             {{ pageNumber }}
           </button>
         </div>
-        <button
-          @click="nextPage"
-          :disabled="page === totalPages"
+        <button @click="nextPage" :disabled="page === totalPages"
           class="flex items-center gap-2 px-3 py-2 text-base font-medium text-center text-slate-900 rounded-lg select-none hover:bg-blue-200 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
           type="button">
           <CaretRightIcon class="w-5 h-5" />
         </button>
-        <button
-          :disabled="page === totalPages"
-          @click="goToPage(totalPages)"
+        <button :disabled="page === totalPages" @click="goToPage(totalPages)"
           class="flex items-center gap-2 px-3 py-2 text-base font-medium text-slate-900 rounded-lg select-none hover:bg-blue-200 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
           type="button">
           <CaretDoubleRightIcon class="w-5 h-5" />
