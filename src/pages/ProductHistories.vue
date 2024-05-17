fdffef37a7c430faba15cb618862ebd2b94de0a4
Matkarim <matkarimov1099@gmail.com>
2024-05-17 15:26:44 +0500

    feat: label print icon located to history page

--- src/pages/ProductHistories.vue
+++ src/pages/ProductHistories.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { computed, h, onMounted, ref, watch } from 'vue'
-import { computed, h, ref, watch } from 'vue'
 import SearchIcon from '../assets/icons/SearchIcon.vue'
 import Spinners270RingIcon from '../assets/icons/Spinners270RingIcon.vue'
 import HistoryTable from '../components/common/HistoryTable.vue'
