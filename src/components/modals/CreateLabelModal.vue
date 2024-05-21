633101b7dde85f5bd1a90fd27fae70741c4ddb54
Matkarim <matkarimov1099@gmail.com>
2024-05-21 16:38:49 +0500

    fix: Bug-while selling product, if barcode doesn't find, it will continue writing to input, then nothing find

--- src/components/modals/CreateLabelModal.vue
+++ src/components/modals/CreateLabelModal.vue
@@ -89,7 +89,7 @@ const createLabel = () => {
           totalPrice: product.price * Number.parseInt(submitData.quantity),
           productionDate: product.productionDate,
           expirationDate: product.expirationDate,
-        })
+        }),
       )
       .then(async () => {
         toast.success(t('labelCreatedSuccessfully'))
@@ -115,25 +115,34 @@ const searchProducts = () => {
       ProductService.getProductsDetails(
         cleanObjectEmptyFields({
           barcode: search.value,
-        })
+        }),
       ).then((res) => {
         isLoading.value = false
+        if (res.data.length === 0) {
+          toast.error(t('productNotFound'))
+          search.value = ''
+        }
         productBarcodes.value = res.data
       })
     } else if (!isNaN(search.value) && Number.isInteger(+search.value)) {
       ProductService.getProductsDetails(
         cleanObjectEmptyFields({
           serialId: +search.value,
-        })
+        }),
       ).then((res) => {
-        isLoading.value = false
-        productBarcodes.value = res.data
+        if (res.data.length === 0) {
+          toast.error(t('productNotFound'))
+          search.value = ''
+        } else {
+          isLoading.value = false
+          productBarcodes.value = res.data
+        }
       })
     } else {
       ProductService.getProductsDetails(
         cleanObjectEmptyFields({
           name: search.value,
-        })
+        }),
       ).then((res) => {
         isLoading.value = false
         productBarcodes.value = res.data
@@ -161,7 +170,7 @@ watch(
       submitData.name = data?.name
     }
   },
-  { deep: true }
+  { deep: true },
 )
 
 
@@ -180,11 +189,12 @@ watch(
     } else {
       submitData.totalPrice = null
     }
-  }
+  },
 )
 </script>
 <template>
-  <CModal :is-open="useModalStore().isOpenCreateLabelModal" v-if="useModalStore().isOpenCreateLabelModal" @close="closeModal">
+  <CModal :is-open="useModalStore().isOpenCreateLabelModal" v-if="useModalStore().isOpenCreateLabelModal"
+          @close="closeModal">
     <template v-slot:header>
       {{ $t('createLabel') }}
     </template>
@@ -193,36 +203,41 @@ watch(
         <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
           <SearchIcon class="w-5 h-5 text-slate-400" />
         </div>
-        <input type="search" v-model="search" ref="onSearchFocus" v-on:keypress="whenPressEnter($event)" class="bg-slate-100 border-none text-slate-900 rounded-lg w-full h-12 pl-10 placeholder-slate-400 placeholder:text-sm md:placeholder:text-lg" :placeholder="t('searchByProductNameOrBarcode')" />
+        <input type="search" v-model="search" ref="onSearchFocus" v-on:keypress="whenPressEnter($event)"
+               class="bg-slate-100 border-none text-slate-900 rounded-lg w-full h-12 pl-10 placeholder-slate-400 placeholder:text-sm md:placeholder:text-lg"
+               :placeholder="t('searchByProductNameOrBarcode')" />
         <div class="absolute inset-y-0 right-0 flex items-center space-x-2">
-          <div @click="useModalStore().openCameraScannerModal()" class="flex items-center justify-center w-8 h-8 rounded-lg hover:bg-white cursor-pointer">
+          <div @click="useModalStore().openCameraScannerModal()"
+               class="flex items-center justify-center w-8 h-8 rounded-lg hover:bg-white cursor-pointer">
             <BarcodeIcon class="w-6 h-6 text-slate-900" />
           </div>
-          <button type="button" @click="searchProducts()" class="px-4 bg-[#0167F3] text-white rounded-lg text-base h-full md:text-lg cursor-pointer">
+          <button type="button" @click="searchProducts()"
+                  class="px-4 bg-[#0167F3] text-white rounded-lg text-base h-full md:text-lg cursor-pointer">
             {{ $t('search') }}
           </button>
         </div>
 
         <div v-if="productBarcodes.length > 0" class="absolute top-16 left-0 bg-transparent w-full space-y-2 z-[2000]">
           <ScrollPanel style="height: 600px;">
-          <div v-for="(product, idx) in productBarcodes" :key="idx" @click="selectedProduct(product)" class="flex items-center justify-between bg-white border shadow-sm rounded-xl px-3 py-2 w-full cursor-pointer hover:bg-slate-100">
-            <div class="flex items-center space-x-3">
-              <div class="flex items-center justify-center bg-slate-200 w-10 h-10 rounded-lg">
-                <ImageIcon class="text-gray-500 w-8 h-8" />
+            <div v-for="(product, idx) in productBarcodes" :key="idx" @click="selectedProduct(product)"
+                 class="flex items-center justify-between bg-white border shadow-sm rounded-xl px-3 py-2 w-full cursor-pointer hover:bg-slate-100">
+              <div class="flex items-center space-x-3">
+                <div class="flex items-center justify-center bg-slate-200 w-10 h-10 rounded-lg">
+                  <ImageIcon class="text-gray-500 w-8 h-8" />
+                </div>
+                <div>
+                  <div class="text-base font-semibold text-gray-800">
+                    {{ product?.name + ' - ' + product?.packaging }}
+                  </div>
+                  <div class="text-base font-medium text-gray-500">
+                    {{ product?.type_code }}
+                  </div>
+                </div>
               </div>
-              <div>
-                <div class="text-base font-semibold text-gray-800">
-                  {{ product?.name + ' - ' + product?.packaging }}
-                </div>
-                <div class="text-base font-medium text-gray-500">
-                  {{ product?.type_code }}
-                </div>
+              <div class="text-base font-semibold text-gray-800">
+                {{ product?.barcode }}
               </div>
             </div>
-            <div class="text-base font-semibold text-gray-800">
-              {{ product?.barcode }}
-            </div>
-          </div>
           </ScrollPanel>
         </div>
       </div>
@@ -233,13 +248,17 @@ watch(
               {{ $t('productName') }}
               <span class="text-red-500 mr-2">*</span>
             </label>
-            <input id="name" disabled type="text" v-model="submitData.name" class="bg-slate-100 border-none text-slate-900 rounded-lg w-full py-2.5 placeholder-slate-400 placeholder:text-sm md:placeholder:text-lg" :placeholder="t('productName')" />
+            <input id="name" disabled type="text" v-model="submitData.name"
+                   class="bg-slate-100 border-none text-slate-900 rounded-lg w-full py-2.5 placeholder-slate-400 placeholder:text-sm md:placeholder:text-lg"
+                   :placeholder="t('productName')" />
           </div>
           <div class="flex-1">
             <label for="barcode" class="text-base md:text-lg font-medium">
               {{ $t('barcode') }}
             </label>
-            <input id="barcode" disabled type="text" v-model="submitData.barcode" class="bg-slate-100 border-none text-slate-900 rounded-lg w-full py-2.5 placeholder-slate-400 placeholder:text-sm md:placeholder:text-lg" :placeholder="t('barcode')" />
+            <input id="barcode" disabled type="text" v-model="submitData.barcode"
+                   class="bg-slate-100 border-none text-slate-900 rounded-lg w-full py-2.5 placeholder-slate-400 placeholder:text-sm md:placeholder:text-lg"
+                   :placeholder="t('barcode')" />
           </div>
         </div>
         <div class="flex flex-col md:flex-row md:items-center space-y-2 md:space-y-0 md:space-x-4">
@@ -248,7 +267,9 @@ watch(
               {{ $t('quantity') }} {{ productBarcode.saleType && `(${productBarcode.saleType})` }}
               <span class="text-red-500 mr-2">*</span>
             </label>
-            <input  id="quantity" type="number" v-model="submitData.quantity" class="bg-slate-100 border-none text-slate-900 rounded-lg w-full h-11 placeholder-slate-400 placeholder:text-sm md:placeholder:text-lg" :placeholder="t('enterProductQuantity')" />
+            <input id="quantity" type="number" v-model="submitData.quantity"
+                   class="bg-slate-100 border-none text-slate-900 rounded-lg w-full h-11 placeholder-slate-400 placeholder:text-sm md:placeholder:text-lg"
+                   :placeholder="t('enterProductQuantity')" />
           </div>
           <div class="flex-1 space-y-1">
             <label for="totalPrice" class="text-base md:text-lg font-medium">
@@ -258,24 +279,29 @@ watch(
             <money3 v-model.number="submitData.totalPrice" v-bind="moneyConf" id="totalPrice"
                     class="border-none text-right text-gray-500 bg-slate-100 h-11 rounded-lg w-full text-lg">
             </money3>
-             </div>
+          </div>
           <div class="flex-1 space-y-1">
             <label for="count" class="text-base md:text-lg font-medium">
               {{ $t('countOfLabel') }}
               <span class="text-red-500 mr-2">*</span>
             </label>
-            <input id="count" type="number" v-model="submitData.count" class="bg-slate-100 border-none text-slate-900 rounded-lg w-full h-11 placeholder-slate-400 placeholder:text-sm md:placeholder:text-lg" :placeholder="t('enterProductQuantity')" />
+            <input id="count" type="number" v-model="submitData.count"
+                   class="bg-slate-100 border-none text-slate-900 rounded-lg w-full h-11 placeholder-slate-400 placeholder:text-sm md:placeholder:text-lg"
+                   :placeholder="t('enterProductQuantity')" />
           </div>
         </div>
       </div>
     </template>
     <template v-slot:footer>
       <CancelButton @click="closeModal" />
-      <button v-if="isLoading" class="inline-flex items-center justify-center ms-3 text-white bg-blue-600 focus:ring-4 focus:outline-none focus:ring-slate-300 rounded-xl border border-slate-200 text-sm font-medium px-5 py-2.5 focus:z-10 cursor-default">
-        <Spinners270RingIcon class="mr-2 w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300" />
+      <button v-if="isLoading"
+              class="inline-flex items-center justify-center ms-3 text-white bg-blue-600 focus:ring-4 focus:outline-none focus:ring-slate-300 rounded-xl border border-slate-200 text-sm font-medium px-5 py-2.5 focus:z-10 cursor-default">
+        <Spinners270RingIcon
+          class="mr-2 w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300" />
         {{ $t('create') }}
       </button>
-      <button v-else @click="createLabel()" type="button" class="ms-3 text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-slate-300 rounded-xl border border-slate-200 text-sm font-medium px-5 py-2.5 focus:z-10">
+      <button v-else @click="createLabel()" type="button"
+              class="ms-3 text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-slate-300 rounded-xl border border-slate-200 text-sm font-medium px-5 py-2.5 focus:z-10">
         {{ $t('create') }}
       </button>
     </template>
