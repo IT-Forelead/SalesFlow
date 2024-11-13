b6c204f218f02dc5c917456c77d02ad6ac40e287
aziz <19atabaev.aziz97@gmail.com>
2024-11-13 16:45:09 +0500

    Dynamic sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -107,28 +107,30 @@ onMounted(() => {
       class="flex justify-between flex-col bg-slate-100 dark:bg-slate-900 absolute z-40 left-0 top-0 lg:static border-r lg:left-auto lg:top-auto lg:translate-x-0 min-h-screen w-72 md:w-64 2xl:w-72 shrink-0 transition-all duration-200 ease-in-out"
       :class="useSidebarStore().isOpenSidebar ? 'translate-x-0' : '-translate-x-64'">
       <div>
-      <div class="h-20 flex justify-around">
-        <div class="flex space-x-1 items-center justify-center">
-          <img src="/images/logo.svg" class="w-12 h-12" alt="#">
-          <div class="text-3xl font-extrabold cursor-default">
-            <span class="text-[#0167f3]">Sales</span>
-            <span class="dark:text-white">Flow</span>
+        <div class="h-20 flex justify-around">
+          <div class="flex space-x-1 items-center justify-center">
+            <img src="/images/logo.svg" class="w-12 h-12" alt="#">
+            <div class="text-3xl font-extrabold cursor-default">
+              <span class="text-[#0167f3]">Sales</span>
+              <span class="dark:text-white">Flow</span>
+            </div>
+          </div>
+          <div class="flex items-center justify-end">
+            <XIcon
+              class="w-6 h-6 mt-1 hover:text-[#0167f3] dark:text-white dark:hover:bg-gray-500 rounded-md cursor-pointer"
+              @click="closeBar" />
           </div>
         </div>
-        <div class="flex items-center justify-end">
-          <XIcon class="w-6 h-6 mt-1 hover:text-[#0167f3] dark:text-white dark:hover:bg-gray-500 rounded-md cursor-pointer" @click="closeBar"/>
-        </div>
-      </div>
-      <ProfileDropDown />
-        <div class="space-y-8 overflow-y-auto xl:h-[67vh] lg:h-[50vh] py-4">
-          <div class="relative space-y-1 md:overflow-visible overflow-y-auto max-h-[400px]">
-          <router-link v-if="navigationGuard('dashboard')" to="/dashboard" @click="selectPage()" active-class="active"
+        <ProfileDropDown />
+        <div class="max-h-svh flex pb-[18rem] flex-col">
+          <div class="relative space-y-1 py-1 overflow-auto md:max-h-screen">
+            <router-link v-if="navigationGuard('dashboard')" to="/dashboard" @click="selectPage()" active-class="active"
             class="relative h-10 flex items-center w-full hover:bg-blue-300/10 hover:text-blue-600 py-5 text-zinc-400 dark:text-zinc-200 text-lg font-medium space-x-4 cursor-pointer transition-colors duration-300">
             <div class="w-1.5 h-12 rounded-r-xl first-child-bg-color mr-2"></div>
             <div class="flex h-10 items-center justify-center rounded-xl w-10 second-child-bg-color">
               <HouseIcon class="w-6 h-6" />
             </div>
-            <div>
+            <div class="w-full">
               {{ $t('dashboard') }}
             </div>
           </router-link>
@@ -138,7 +140,7 @@ onMounted(() => {
             <div class="flex h-10 items-center justify-center rounded-xl w-10 second-child-bg-color">
               <MoneyIcon class="w-6 h-6" />
             </div>
-            <div>
+            <div class="w-full">
               {{ $t('sale') }}
             </div>
           </router-link>
@@ -148,7 +150,7 @@ onMounted(() => {
             <div class="flex h-10 items-center justify-center rounded-xl w-10 second-child-bg-color">
               <PhShoppingCart class="w-6 h-6" />
             </div>
-            <div>
+            <div class="w-full">
               {{ $t('products') }}
             </div>
           </router-link>
@@ -159,7 +161,7 @@ onMounted(() => {
             <div class="flex h-10 items-center justify-center rounded-xl w-10 second-child-bg-color">
               <ProductHistoryIcon class="w-6 h-6" />
             </div>
-            <div>
+            <div class="w-full">
               {{ $t('incomeExpense') }}
             </div>
           </router-link>
@@ -170,7 +172,7 @@ onMounted(() => {
             <div class="flex h-10 items-center justify-center rounded-xl w-10 second-child-bg-color">
               <PhShoppingCart class="w-6 h-6" />
             </div>
-            <div>
+            <div class="w-full">
               {{ $t('upcomingProducts') }}
             </div>
           </router-link>
@@ -181,7 +183,7 @@ onMounted(() => {
             <div class="flex h-10 items-center justify-center rounded-xl w-10 second-child-bg-color">
               <PhPercent class="w-6 h-6" />
             </div>
-            <div>
+            <div class="w-full">
               {{ $t('discount') }}
             </div>
           </router-link>
@@ -192,7 +194,7 @@ onMounted(() => {
             <div class="flex h-10 items-center justify-center rounded-xl w-10 second-child-bg-color">
               <TicketSaleIcon class="w-6 h-6" />
             </div>
-            <div>
+            <div class="w-full">
               {{ $t('cashbackHistories') }}
             </div>
           </router-link>
@@ -202,7 +204,7 @@ onMounted(() => {
             <div class="flex h-10 items-center justify-center rounded-xl w-10 second-child-bg-color">
               <PhShoppingCart class="w-6 h-6" />
             </div>
-            <div>
+            <div class="w-full">
               {{ $t('sales') }}
             </div>
           </router-link>
@@ -212,7 +214,7 @@ onMounted(() => {
             <div class="flex h-10 items-center justify-center rounded-xl w-10 second-child-bg-color">
               <StoreIcon class="w-6 h-6" />
             </div>
-            <div>
+            <div class="w-full">
               {{ $t('shops') }}
             </div>
           </router-link>
@@ -222,7 +224,7 @@ onMounted(() => {
             <div class="flex h-10 items-center justify-center rounded-xl w-10 second-child-bg-color">
               <UsersIcon class="w-6 h-6" />
             </div>
-            <div>
+            <div class="w-full">
               {{ $t('users') }}
             </div>
           </router-link>
@@ -233,7 +235,7 @@ onMounted(() => {
             <div class="flex h-10 items-center justify-center rounded-xl w-10 second-child-bg-color">
               <PhBarcodeIcon class="w-6 h-6" />
             </div>
-            <div>
+            <div class="w-full">
               {{ $t('barcodes') }}
             </div>
           </router-link>
@@ -244,7 +246,7 @@ onMounted(() => {
             <div class="flex items-center justify-center rounded-xl w-10 h-10 second-child-bg-color">
               <PhBarcodeIcon class="w-6 h-6" />
             </div>
-            <div>
+            <div class="w-full">
               {{ $t('duplicates') }}
             </div>
           </router-link>
@@ -255,7 +257,7 @@ onMounted(() => {
             <div class="flex h-10 items-center justify-center rounded-xl w-10 second-child-bg-color">
               <SettingsIcon class="w-6 h-6" />
             </div>
-            <div>
+            <div class="w-full">
               {{ $t('settings') }}
             </div>
           </router-link>
@@ -265,7 +267,7 @@ onMounted(() => {
             <div class="flex h-10 items-center justify-center rounded-xl w-10 second-child-bg-color">
               <ClientCorpIcon class="w-6 h-6" />
             </div>
-            <div>
+            <div class="w-full">
               {{ $t('corporateClients') }}
             </div>
           </router-link>
@@ -275,7 +277,7 @@ onMounted(() => {
             <div class="flex h-10 items-center justify-center rounded-xl w-10 second-child-bg-color">
               <ClientIcon class="w-6 h-6" />
             </div>
-            <div>
+            <div class="w-full">
               {{ $t('clients') }}
             </div>
           </router-link>
@@ -285,17 +287,17 @@ onMounted(() => {
             <div class="flex h-10 items-center justify-center rounded-xl w-10 second-child-bg-color">
               <PhUsersThree class="w-6 h-6" />
             </div>
-            <div>
+            <div class="w-full">
               {{ $t('agents') }}
             </div>
           </router-link>
           <router-link v-if="navigationGuard('view_agents')" to="/vouchers" @click="selectPage()" active-class="active"
-          class="relative h-10 flex items-center w-full hover:bg-blue-300/10 hover:text-blue-600 py-5 text-zinc-400 dark:text-white text-lg font-medium space-x-4 cursor-pointer transition-colors duration-300">
+          class="relative h-10 flex items-center w-full hover:bg-blue-300/10 hover:text-blue-600 py-5 text-zinc-400 dark:text-zinc-200 text-lg font-medium space-x-4 cursor-pointer transition-colors duration-300">
             <div class="w-1.5 h-12 rounded-r-xl first-child-bg-color mr-2"></div>
             <div class="flex h-10 items-center justify-center rounded-xl w-10 second-child-bg-color">
               <PhUsersThree class="w-6 h-6" />
             </div>
-            <div>
+            <div class="w-full">
               {{ $t('vouchers') }}
             </div>
           </router-link>
@@ -305,7 +307,7 @@ onMounted(() => {
             <div class="flex h-10 items-center justify-center rounded-xl w-10 second-child-bg-color">
               <PhPriceList class="w-6 h-6" />
             </div>
-            <div>
+            <div class="w-full">
               {{ $t('priceLists') }}
             </div>
           </router-link>
@@ -315,7 +317,7 @@ onMounted(() => {
             <div class="flex h-10 items-center justify-center rounded-xl w-10 second-child-bg-color">
               <FileLinearIcon class="w-6 h-6" />
             </div>
-            <div>
+            <div class="w-full">
               {{ $t('wishes') }}
             </div>
           </router-link>
@@ -325,7 +327,7 @@ onMounted(() => {
             <div class="flex h-10 items-center justify-center rounded-xl w-10 second-child-bg-color">
               <PhLockKey class="w-6 h-6" />
             </div>
-            <div>
+            <div class="w-full">
               {{ $t('ipBanned') }}
             </div>
           </router-link>
@@ -335,7 +337,7 @@ onMounted(() => {
             <div class="flex h-10 items-center justify-center rounded-xl w-10 second-child-bg-color">
               <InvestorIcon class="w-6 h-6" />
             </div>
-            <div>
+            <div class="w-full">
               {{ $t('investors') }}
             </div>
           </router-link>
@@ -345,7 +347,7 @@ onMounted(() => {
             <div class="flex h-10 items-center justify-center rounded-xl w-10 second-child-bg-color">
               <InvestIcon class="w-6 h-6" />
             </div>
-            <div>
+            <div class="w-full">
               {{ $t('invests') }}
             </div>
           </router-link>
@@ -355,51 +357,46 @@ onMounted(() => {
             <div class="flex h-10 items-center justify-center rounded-xl w-10 second-child-bg-color">
               <InvestPlanIcon class="w-6 h-6" />
             </div>
-            <div>
+            <div class="w-full">
               {{ $t('investPlans') }}
             </div>
           </router-link>
-        </div>
-      </div>
-      </div>
-      <div class="absolute bottom-0 w-full mb-3 space-y-4">
-        <div class="px-4">
-          <button @click="toggleWishToBuyProductModal"
-            class="w-full py-2 px-4 rounded-lg text-white text-base bg-blue-500 cursor-pointer hover:bg-blue-600">
-            Istak qo'shish
-          </button>
-          <OverlayPanel ref="wishToBuyProductModal">
-            <div class="w-96">
-              <input v-model="wishToBuyProductName" id="login" type="text" v-on:keypress="whenPressEnter($event)"
-                class="bg-slate-100 border-none dark:bg-slate-700 text-slate-900 dark:text-white rounded-lg w-full py-2.5 placeholder-slate-400"
-                :placeholder="t('enterProductName')">
+
+
+
+          </div>
+          <div class="absolute bottom-32 h-min w-full space-y-4 pb-3 bg-slate-100 dark:bg-slate-900">
+            <div class="px-4">
+              <button @click="toggleWishToBuyProductModal"
+                class="w-full py-2 rounded-lg text-white text-base bg-blue-500 cursor-pointer hover:bg-blue-600">
+                Istak qo'shish
+              </button>
+              <OverlayPanel ref="wishToBuyProductModal">
+                <div class="w-96">
+                  <input v-model="wishToBuyProductName" id="login" type="text" v-on:keypress="whenPressEnter($event)"
+                    class="bg-slate-100 border-none dark:bg-slate-700 text-slate-900 dark:text-white rounded-lg w-full py-2.5 placeholder-slate-400"
+                    :placeholder="t('enterProductName')">
+                </div>
+              </OverlayPanel>
             </div>
-          </OverlayPanel>
-        </div>
-        <div class="flex items-center space-x-3 px-4">
-          <SelectOptionLanguage />
-          <SelectOptionTheme />
-          <!-- <div
-            class="flex-1 flex items-center justify-between rounded-lg bg-white dark:bg-slate-900 p-2 cursor-pointer hover:bg-gray-100">
-            <div class="flex items-center space-x-1">
-              <SunIcon class="w-5 h-5 text-gray-500 dark:text-zinc-300" />
-              <span>Light</span>
+            <div class="flex items-center space-x-3 px-4">
+              <SelectOptionLanguage />
+              <SelectOptionTheme />
             </div>
-            <CaretDownIcon class="w-3 h-3" />
-          </div> -->
-        </div>
-        <div class="text-xs text-center text-slate-400 dark:text-white">
-          Copyright &copy; {{ new Date().getFullYear() }} <a href="https://it-forelead.uz"
-            class="hover:underline">IT-Forelead</a>.
-          <br />
-          All Rights Reserved.
+            <div class="text-xs text-center text-slate-400 dark:text-white">
+              Copyright &copy; {{ new Date().getFullYear() }} <a href="https://it-forelead.uz"
+                class="hover:underline">IT-Forelead</a>.
+              <br />
+              All Rights Reserved.
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
 <style>
-.dark .p-overlaypanel-content{
+.dark .p-overlaypanel-content {
   background-color: rgb(30 41 59) !important;
 }
 </style>
